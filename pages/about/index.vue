06003ae4fcfaeb8dbca7d07ce0bc7870784fb575
thunder <joseph1211@126.com>
2023-05-19 21:45:20 +0800

    init repository

---
+++ pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtLink to="/">Home</NuxtLink>
+  <NuxtLink to="/about">About</NuxtLink>
+  <div>About us</div>
+</template>
+
+<style lang="scss" scoped>
+.router-link-active{
+  color: #d86f4b;
+  position: relative;
+  &::after{
+    content: '';
+    display: block;
+    width: 100%;
+    height: 2px;
+    border-radius: 10px;
+    background-color: #d86f4b;
+    position: absolute;
+    left: 0;
+    bottom: -5px;
+  }
+}
+</style>
