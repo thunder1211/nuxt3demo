8b511bc7dc4d44aab57c35a47447ad2938f55078
thunder <joseph1211@126.com>
2023-05-20 17:45:01 +0800

    _nuxt下划线,gh-pages出问题,所以改成assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/nuxt3demo'
+    baseURL: '/nuxt3demo',
+    buildAssetsDir: "/assets/",
   },
   css: ['assets/scss/index.scss'],
 })
